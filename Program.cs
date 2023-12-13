637911999894239bc4c7b8a61c9be0f3bebef0de
student <student@NB-SVC04>
2023-12-13 17:49:56 +0100

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("Zadejte jméno");
+string jmeno = Console.ReadLine();
+
+if (jmeno == "Vita") 
+{
+    Console.WriteLine("je frajer");
+}
+else if(jmeno == "jakub")
+{
+    Console.WriteLine("je divnej")
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Neexistuje");
+}
+
